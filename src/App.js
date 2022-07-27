8b9e057401f6a8bc04110b480a294974ee287353
Muhammad Kashaf <kashafahmed72@gmail.com>
2022-07-27 18:53:09 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import { FormProvider, useForm } from "react-hook-form";
-import { Route } from "react-router-dom";
-import { BrowserRouter, Router, Routes } from "react-router-dom";
 import RegistraionForm from "./screens/RegistraionForm";
-// import Autocompletes from "./component/Autocompletes";
-// import AddressForm from '../component/formComponents/AddressForm';
-// import ContactForm from '../component/formComponents/ContactForm';
-// import PersonalInfo from '../component/formComponents/PersonalInfo';
 
 function App() {
   const useFormMethods = useForm();
   return (
-    //#region Router
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<RegistraionForm />} />
-    //     <Route path="/ContactForm" element={<ContactForm />} />
-    //     <Route path="/AddressForm" element={<AddressForm />} />
-    //     <Route path="/PersonalInfo" element={<PersonalInfo />} />
-    //   </Routes>
-    // </BrowserRouter>
-    //#endregion
-
     <FormProvider {...useFormMethods}>
       <RegistraionForm />
     </FormProvider>
