99ada6659f7459f58160e2568b66c3cbe3aa712c
Muhammad Kashaf <kashafahmed72@gmail.com>
2022-07-28 18:27:40 +0500

    change

--- src/component/formComponents/PersonalInfo.jsx
+++ src/component/formComponents/PersonalInfo.jsx
@@ -1,31 +1,94 @@
 import styles from "../../styles/formStyle.module.css";
 import { Box, Button, TextField, Typography } from '@mui/material';
-import { useFormContext } from "react-hook-form";
+import { Controller, useFormContext, useController } from "react-hook-form";
 
 const PersonalInfo = () => {
-    const { register, formState: { errors } } = useFormContext();
-
+    const { control } = useFormContext();
+    // const {} =useController();
     return (
         <section className={styles.formStyle}>
             <Typography variant="h4" align="center">
                 Personal Form
             </Typography>
             <Box marginTop={2} marginBottom={2}>
-                <TextField label="First Name" fullWidth {...register("firstname", { required: true })} />
-                {errors.firstname && <small style={{ color: "red" }}>First Name is required</small>}
+                <Controller
+                    name="firstname"
+                    control={control}
+                    rules={{ required: { value: true, message: "First Name is required" } }}
+                    render={
+                        ({
+                            field: { onChange, onBlur, value, name, ref },
+                            fieldState: { invalid, isTouched, isDirty, error },
+                            formState: { errors },
+                        }) => (
+                            <>
+                                <TextField label="First Name" fullWidth />
+                                {errors.firstname && <small style={{ color: "red" }}>{errors.firstname.message}</small>}
+                            </>
+                        )
+                    }
+                />
+
+                {/* <TextField label="First Name" fullWidth {...register("firstname", { required: true })} /> */}
+                {/* {errors.firstname && <small style={{ color: "red" }}>First Name is required</small>} */}
             </Box>
 
-            <Box marginTop={2} marginBottom={2}>
+            {/* <Box marginTop={2} marginBottom={2}>
                 <TextField label="Last Name" fullWidth {...register("lastname", { required: true })} />
                 {errors.lastname && <small style={{ color: "red" }}>Last Name is required</small>}
+            </Box> */}
+
+            <Box marginTop={2} marginBottom={2}>
+                <Controller
+                    name="lastname"
+                    control={control}
+                    rules={{ required: { value: true, message: "Last Name is required" } }}
+                    render={
+                        ({
+                            field: { onChange, onBlur, value, name, ref },
+                            fieldState: { invalid, isTouched, isDirty, error },
+                            formState: { errors },
+                        }) => (
+                            <>
+                                <TextField label="Last Name" fullWidth />
+                                {errors.lastname && <small style={{ color: "red" }}>{errors.lastname.message}</small>}
+                            </>
+                        )
+                    }
+                />
+
+                {/* <TextField label="First Name" fullWidth {...register("firstname", { required: true })} /> */}
+                {/* {errors.firstname && <small style={{ color: "red" }}>First Name is required</small>} */}
             </Box>
 
-            <Box marginBottom={2}>
+            {/* <Box marginBottom={2}>
                 <TextField label="Age" type="number" fullWidth {...register("age", {
                     required: { value: true, message: "Age is required" },
                     maxLength: { value: 2, message: "Wrong Age" },
                 })} />
                 {errors.age && <small style={{ color: "red" }}>{errors.age.message}</small>}
+            </Box> */}
+            <Box marginTop={2} marginBottom={2}>
+                <Controller
+                    name="age"
+                    control={control}
+                    rules={{ required: { value: true, message: "Age is required" }, maxLength: { value: 2, message: "Wrong Age" } }}
+                    render={
+                        ({
+                            field: { onChange, onBlur, value, name, ref },
+                            fieldState: { invalid, isTouched, isDirty, error },
+                            formState: { errors },
+                        }) => (
+                            <>
+                                <TextField label="Age" fullWidth type={'number'} />
+                                {errors.age && <small style={{ color: "red" }}>{errors.age.message}</small>}
+                            </>
+                        )
+                    }
+                />
+
+                {/* <TextField label="First Name" fullWidth {...register("firstname", { required: true })} /> */}
+                {/* {errors.firstname && <small style={{ color: "red" }}>First Name is required</small>} */}
             </Box>
 
         </section>
